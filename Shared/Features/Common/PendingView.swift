fc70d032a16389dddde56890895a819aed2a8724
Dev Agani <dev.things.ke@gmail.com>
2022-06-19 10:16:30 +0200

    use method initialization to communicate intent

--- Shared/Features/Common/PendingView.swift
+++ Shared/Features/Common/PendingView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct PendingView<Content: View>: View {
-    var isRedacted = true
+    var isRedacted: Bool
     let content: Content
 
-    init(isRedacted: Bool, @ViewBuilder content: () -> Content) {
+    init(isRedacted: Bool = true, @ViewBuilder content: () -> Content) {
         self.content = content()
         self.isRedacted = isRedacted
     }
