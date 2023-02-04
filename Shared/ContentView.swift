96af8f1c22c1caccbcff5dca254ab193bade34b3
George Nyakundi <georgen@backbase.com>
2023-02-04 20:53:14 +0100

    Uncomment preview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -77,9 +77,9 @@ struct ContentView: View {
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
