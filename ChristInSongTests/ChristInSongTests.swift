9ad81a1e4a7fe0a97c09ca86ca894a46a93080c5
George Nyakundi <georgen@backbase.com>
2023-03-10 15:47:33 +0100

    Add a testing scheme and a dummy test

---
+++ ChristInSongTests/ChristInSongTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChristInSongTests.swift
+//  ChristInSongTests
+//
+//  Created by George Nyakundi on 10/03/2023.
+//
+
+import XCTest
+
+final class ChristInSongTests: XCTestCase {
+
+    func testDummy() {
+        let isTrue = true
+        XCTAssertTrue(isTrue, "Dummy test")
+    }
+}
