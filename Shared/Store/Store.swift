c12ec0f1579331183aefb95cb4624946bca25fc1
George Nyakundi <georgen@backbase.com>
2023-03-27 21:24:23 +0200

    Add comments to the `Store` protocol

--- Shared/Store/Store.swift
+++ Shared/Store/Store.swift
@@ -8,16 +8,36 @@
 import Foundation
 import CoreData
 
+/// Conform to this protocol to perfom any store operations within the CIS applicaiton
 public protocol Store {
     /// Retrieve all books from the store
-    /// - Returns: an array of ``StoreBook``
+    /// - Returns: a list of ``StoreBook``
     func retrieveAllBooks() -> [StoreBook]
+    /// Retrieves all collections from store
+    /// - Returns: a list of ``StoreCollection``
     func retrieveAllCollections() -> [StoreCollection]
+    /// Retrieves all hymns for a particular book
+    /// - Parameter book: the `key` for the book
+    /// - Returns: a list of store hymns when found and nil when no book is available
     func retrieveHymns(from book: String) -> [StoreHymn]?
+    /// Sets a book as the selected book, by default during app launch hymns are fetched from this book
+    /// - Parameter bookName: `key` to the book
     func setSelectedBook(to bookName: String)
+    /// Retrieves the selected bookKey
+    /// - Returns: book `key`
     func retrieveSelectedBook() -> String?
+    /// Creates a new collection
+    /// - Parameters:
+    ///   - title: `title` to the the collection
+    ///   - about: an optional `description` to the collection
     func createCollection(with title: String, and about: String?)
+    /// Deletes a collection from the store
+    /// - Parameter id: the unique `id` (UUID) of the collection
     func removeCollection(with id: UUID)
+    /// Toggle the presence of a hymn in a collection. (Removes/Add)
+    /// - Parameters:
+    ///   - hymn: hymn
+    ///   - collection: collection
     func toggle(hymn: StoreHymn,in collection: StoreCollection)
 }
 
