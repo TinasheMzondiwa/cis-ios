278f242d8eedd9ea67beb4a2dbe5ae2809fc7c5f
George Nyakundi <georgen@backbase.com>
2023-03-28 22:02:28 +0200

    More cleanup

--- Shared/ChristInSongApp.swift
+++ Shared/ChristInSongApp.swift
@@ -7,45 +7,6 @@
 
 import SwiftUI
 
-//@main
-//struct ChristInSongApp: App {
-//
-//    let viewModel: CISAppViewModel
-//
-//    init() {
-//        let store = CISCoreDataStore()
-//        viewModel = CISAppViewModel(store: store)
-//    }
-//
-//    var body: some Scene {
-//        WindowGroup {
-//            TabView {
-//                HymnsView()
-//                    .tabItem {
-//                        NavLabel(item: NavItem.hymns)
-//                    }
-//                    .tag(0)
-//                CollectionsView()
-//                    .tabItem { NavLabel(item: NavItem.collections)
-//                    }
-//                    .tag(1)
-//                SupportView()
-//                    .tabItem {
-//                        NavLabel(item: NavItem.support)
-//                    }
-//                    .tag(2)
-//                InfoView()
-//                    .tabItem {
-//                        NavLabel(item: NavItem.info)
-//                    }
-//                    .tag(3)
-//            }
-//            .environmentObject(viewModel)
-//        }
-//    }
-//}
-
-// MARK: - Uncomment this to test compatibility
 @main
 struct ChristInSongApp: App {
     let viewModel: CISAppViewModel
