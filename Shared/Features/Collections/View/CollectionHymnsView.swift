43c5498d4e90f2e8f84595fabff1401b978aeb4e
George Nyakundi <georgen@backbase.com>
2023-03-29 20:13:12 +0200

    Rename `OldCollectionHymnsView` to `CollectionHymnsView

--- Shared/Features/Collections/View/CollectionHymnsView.swift
+++ Shared/Features/Collections/View/CollectionHymnsView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct OldCollectionHymnsView: View {
+struct CollectionHymnsView: View {
     
     let collection: StoreCollection
     
--- Shared/Features/Collections/View/CollectionsView.swift
+++ Shared/Features/Collections/View/CollectionsView.swift
@@ -61,7 +61,7 @@ struct CollectionsView: View {
                 List {
                     ForEach(filteredCollections, id: \.id) { collection in
                         NavigationLink {
-                            OldCollectionHymnsView(collection: collection)
+                            CollectionHymnsView(collection: collection)
                         } label: {
                             CollectionItemView(item: collection)
                         }
