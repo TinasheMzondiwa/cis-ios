021b2acbbdaf76ab3b249f8cecd0893856fe4b38
Tinashe Mzondiwa <tmzon08@gmail.com>
2026-04-19 17:57:50 +0200

    Update Support screen

--- Shared/Features/Support/SupportView.swift
+++ Shared/Features/Support/SupportView.swift
@@ -6,17 +6,15 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SupportView: View {
     
-    @ObservedObject private var manager = StoreManager.shared
+    @EnvironmentObject var manager: StoreManager
     
     private var idiom : UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
     
-    @State private var showingHUD = false
-    @State private var currState: (message: String, state: AlertState)? = nil
-    
-    private var navTitle: String = NSLocalizedString("Support", comment: "Title")
+    private var navTitle: String = NSLocalizedString("Support.Promo.Title", comment: "Title")
     
     var body: some View {
         if (idiom == .phone) {
@@ -24,22 +22,10 @@ struct SupportView: View {
                 content
                     .navigationTitle(navTitle)
             }
-            .hud(state: currState?.state, isPresented: $showingHUD) {
-                if let data = currState {
-                    Label(data.message, systemImage: data.state.rawValue)
-                        .foregroundColor(data.state == .info ? Color.primary : .white)
-                }
-            }
         } else {
             #if os(iOS)
                 content
                     .navigationTitle(navTitle)
-                    .hud(state: currState?.state, isPresented: $showingHUD) {
-                        if let data = currState {
-                            Label(data.message, systemImage: data.state.rawValue)
-                                                        .foregroundColor(data.state == .info ? Color.primary : .white)
-                        }
-                    }
             #else
                 content
                     .frame(minWidth: 300, idealWidth: 500)
@@ -61,47 +47,17 @@ struct SupportView: View {
                     .multilineTextAlignment(.center)
                     .padding()
                 
-                if manager.donations.isEmpty {
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: WebLink.paypal.rawValue)!)
-                    }, label: {
-                        Text("Donate")
-                            .foregroundColor(.accentColor)
-                            .padding([.top, .bottom], 6)
-                            .padding([.trailing, .leading], 12)
-                            .background(
-                                RoundedRectangle(
-                                    cornerRadius: 20,
-                                    style: .continuous
-                                )
-                                .fill(Color(.secondarySystemBackground))
-                            )
-                    })
-                }
-                
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(alignment: .center, spacing: 20) {
                         Spacer()
                         
-                        ForEach(manager.donations, id: \.self) { product in
-                            Button(action: {
-                                let success = manager.donate(for: product)
-                                debugPrint("State was: \(success)")
-                            }, label: {
-                                Text(product.formattedPrice)
-                                    .font(.system(.headline, design: .rounded))
-                                    .foregroundColor(.accentColor)
-                                    .padding([.top, .bottom], 6)
-                                    .padding([.trailing, .leading], 12)
-                                    .background(
-                                        RoundedRectangle(
-                                            cornerRadius: 20,
-                                            style: .continuous
-                                        )
-                                        .fill(Color(.secondarySystemBackground))
-                                    )
-                            })
+                        ForEach(manager.productIDs, id: \.self) { product in
+                            ProductView(id: product)
+                                .productViewStyle(.regular)
+                                    .padding()
+                                    .cornerRadius(16)
                         }
+                        
                         Spacer()
                         
                     }
@@ -115,27 +71,15 @@ struct SupportView: View {
             }
             
         }
-        .onAppear {
-            manager.getProducts()
+        .sheet(isPresented: $manager.showThankYou) {
+            ThankYouView(productID: manager.lastProductID)
         }
-        .onReceive(manager.purchasePublisher, perform: { data in
-            withAnimation {
-                self.currState = (data.message, data.state.alert)
-                self.showingHUD.toggle()
-            }
-        })
     }
 }
 
 struct SupportView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            SupportView()
-                .previewDevice(PreviewDevice(rawValue: "Iphone 12"))
-            
-            SupportView()
-                .previewDevice(PreviewDevice(rawValue: "Iphone 12 Pro"))
-                .preferredColorScheme(.dark)
-        }
+        SupportView()
+            .environmentObject(StoreManager.shared)
     }
 }
