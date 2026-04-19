a212eac4ebb4422241266799ffcdd7b98e3c087a
Tinashe Mzondiwa <tmzon08@gmail.com>
2026-04-19 03:36:39 +0200

    Move swipe to container view

--- Shared/Features/Hymns/Hymn/HymnView.swift
+++ Shared/Features/Hymns/Hymn/HymnView.swift
@@ -65,45 +65,57 @@ struct HymnView: View {
         let typeface = AppTypeface(rawValue: selectedFontRaw) ?? .defaultTypeface
         
         ZStack {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 20) {
-                    VStack(alignment: .center) {
-                        Text(displayedHymn.number.formatted())
-                        Text(displayedHymn.title)
+            
+            HymnContainerView(
+                previousHymn: displayedHymn.number > 1 ? displayedHymn.number - 1 : nil,
+                nextHymn: displayedHymn.number < vm.hymnsFromSelectedBook.count ? displayedHymn.number + 1 : nil,
+                enabled: true,
+                onPreviousHymn: { swipeTo(direction: .backward) },
+                onNextHymn: { swipeTo(direction: .forward) },
+            ) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .center) {
+                            Text(displayedHymn.number.formatted())
+                            Text(displayedHymn.title)
+                        }
+                        .font(typeface.font(size: fontSize + 4, weight: .bold))
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
+                        .padding(.bottom, 8)
+                        
+                        
+                        VStack(spacing: 20) {
+                            ForEach(displayedHymn.lyrics) { lyric in
+                                if lyric.type == "refrain" {
+                                    ChorusUiView(lines: lyric.lines)
+                                } else {
+                                    VerseUIView(
+                                        index: lyric.index ?? 0,
+                                        lines: lyric.lines
+                                    )
+                                }
+                            }
+                        }
+                        
                     }
-                    .font(typeface.font(size: fontSize + 4, weight: .bold))
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity)
-                    .padding(.bottom, 8)
-                    
-                    
-                    VStack(spacing: 20) {
-                       ForEach(displayedHymn.lyrics) { lyric in
-                           if lyric.type == "refrain" {
-                               ChorusUiView(lines: lyric.lines)
-                           } else {
-                               VerseUIView(
-                                   index: lyric.index ?? 0,
-                                   lines: lyric.lines
-                               )
-                           }
-                       }
-                   }
-                    
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
             }
+            
+            
         }
         .toolbar {
             
             ToolbarSpacer(.flexible)
             
-            ToolbarItem(placement: .title) {
-                Text(displayedBook?.title ?? defaultTitle())
-                    .font(AppTypeface.lato.font(size: 16, weight: .medium))
-                    .padding()
-                    .glassEffect()
+            ToolbarItem(placement: .principal) {
+                HymnalsPickerUIView(
+                    book: displayedBook?.title ?? defaultTitle(),
+                    books: books,
+                    onSelect: { book in setSelectedBook(to: book) }
+                )
             }
             
             ToolbarSpacer(.flexible)
@@ -115,10 +127,6 @@ struct HymnView: View {
                         Label("Add to Collection", systemImage: "text.badge.plus")
                     }
                     
-                    Button(action: { vm.toggleBookSelectionShownFromHymnView() }) {
-                        Label("Switch Hymnal", systemImage: "book.circle")
-                    }
-                    
                     Button(action: {}) {
                         Label(LocalizedStringKey("Text.Format"), systemImage: SFSymbol.textFormat.rawValue)
                     }
@@ -127,17 +135,8 @@ struct HymnView: View {
                 }
             }
         }
-
         .sheet(isPresented: $vm.collectionsSheetShown) {
             AddToCollectionView(hymn: displayedHymn)
-        }
-        .sheet(isPresented: $vm.bookSelectionShownFromHymnView) {
-            HymnalsView(books: books) { book in
-                setSelectedBook(to: book)
-                vm.toggleBookSelectionShownFromHymnView()
-            } dismissAction: {
-                vm.toggleBookSelectionShownFromHymnView()
-            }
         }
         .hud(state: currState?.state, isPresented: $showingHUD) {
             if let data = currState {
@@ -149,15 +148,6 @@ struct HymnView: View {
                 .foregroundColor(data.state == .info ? Color.primary : .white)
             }
         }
-        .overlay(
-            HStack {
-                FloatingActionButtonView(icon: SFSymbol.chevronLeft, size: 40, action: { swipeTo(direction: .backward)})
-                
-                Spacer()
-                
-                FloatingActionButtonView(icon: SFSymbol.chevronRight, size: 40, action: { swipeTo(direction: .forward) })
-            }.padding()
-        )
     }
     
     func defaultTitle() -> String {
